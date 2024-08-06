74007090fdfcab980ecbfd85bb03a2076943020d
ari <40159649+aniknia@users.noreply.github.com>
2024-08-05 23:48:03 -0700

    fix: add winsock2.h and ws2_32.lib for windows support

--- src/main.c
+++ src/main.c
@@ -3,31 +3,36 @@
 #include <string.h>
 
 #if defined(_WIN32) || defined(_WIN64)
-	#define PLATFORM "windows"
+#define PLATFORM "windows"
+#include <winsock2.h>
+#pragma comment(lib, "Ws2_32.lib")
 #elif defined(__linux__)
-	#define PLATFORM "linux"
-	#include <unistd.h>
+#define PLATFORM "linux"
+#include <unistd.h>
 #elif defined(__APPLE__)
-	#define PLATFORM "apple"
-	#include <unistd.h>
+#define PLATFORM "apple"
+#include <unistd.h>
 #else
-	#define PLATFORM "none"
+#define PLATFORM "none"
 #endif
 
 int main()
 {
 	char *username = "";
 	char hostname[1024];
-	
-	gethostname(hostname, sizeof(hostname));
 
 	if (strcmp(PLATFORM, "windows") == 0)
 	{
+		WSADATA wsaData;
+		WSAStartup(MAKEWORD(2, 0), &wsaData);
 		username = getenv("username");
+		gethostname(hostname, sizeof(hostname));
+		WSACleanup();
 	}
 	else
 	{
 		username = getenv("USER");
+		gethostname(hostname, sizeof(hostname));
 	}
 
 	printf("tinyfetch\n");
